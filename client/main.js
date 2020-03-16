5a1949cf0b52bc62bbe650c3be688255bab05a8d
Finn <finnterdal@gmail.com>
2020-03-16 13:07:03 -0500

    adds a form and refresh button

--- client/main.js
+++ client/main.js
@@ -2,12 +2,12 @@ const loadMonsters = require("./loadMonsters");
 const { formatDollars } = require("./utils");
 
 const monsterContainer = document.getElementById("monster-container");
+const refreshMonstersBtn = document.getElementById("refresh-monsters");
 
 async function renderMonsters() {
   const monsters = await loadMonsters();
 
   monsterContainer.innerHTML = "";
-
   monsters.forEach(monster => {
     const monsterBox = document.createElement("DIV");
     monsterBox.classList.add("monster-box");
@@ -15,12 +15,14 @@ async function renderMonsters() {
       monsterBox.classList.add("in-cart");
     }
     monsterBox.innerHTML = `
-      <h2>${monster.name}</h2>
-      <img src="https://robohash.org/${monster.name}?set=set2">
-      <div>${formatDollars(monster.priceInCents)} / hour</div>
+    <h2>${monster.name}</h2>
+    <img src="https://robohash.org/${monster.name}?set=set2">
+    <div>${formatDollars(monster.priceInCents)} / hour</div>
     `;
     monsterContainer.appendChild(monsterBox);
   });
 }
+
+refreshMonstersBtn.onclick = renderMonsters;
 
 renderMonsters();
